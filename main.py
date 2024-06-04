395140a66a3d069ee0b08b932152a66f121b680b
Sander Bos <sanderb0ss@live.nl>
2024-06-04 14:44:13 +0200

    Set_Up and basic regression api

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def hello():
-    return "hallo"
+from flask import Flask
+from flask_session import Session
+from back_end import regression_bp
 
-hello()
+app = Flask(__name__)
+
+app.register_blueprint(regression_bp)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
