124248e7e7d31f8de7cbd0ea105bdcedeb1d9852
Sander Bos <sanderb0ss@live.nl>
2024-06-26 11:17:21 +0200

    Remove Connections
    - can now remove connections
    - Password is hidden on the connection page

--- backEnd/database/databaseEndpoints.py
+++ backEnd/database/databaseEndpoints.py
@@ -14,10 +14,6 @@ def getConnections():
 
     connectionList = dbConncetions.query.all()
     connections_dict_list = [connection.to_dict() for connection in connectionList]
-
-    print(connections_dict_list)
-    print(type(connections_dict_list))
-    
     answer = json.dumps(connections_dict_list)
 
     return answer
@@ -25,18 +21,31 @@ def getConnections():
 
 @databaseBP.route('/addConnection', methods=['POST'])
 def addConnection():
-    body = request.get_json()
-    print(body)
     try:
+        body = request.get_json()
+        connectionID = body['connectionName']
         Host = body['connectionHost']
         Port = body['connectionPort']
         Database = body['connectionDBName']
         UserName = body['connectionUser']
         Password = body['connectionPassword']
         DataBaseType = body['connectionType']
-        new_connection = dbConncetions(Host=Host, Port=Port, DataBase=Database, UserName=UserName, Password=Password, DatabaseType=DataBaseType)
+        new_connection = dbConncetions(connectionID=connectionID, Host=Host, Port=Port, DataBase=Database, UserName=UserName, Password=Password, DatabaseType=DataBaseType)
         db.session.add(new_connection)
         db.session.commit()
         return "Connection added successfully!", 200
+    except Exception as e:
+        return str(e), 500
+
+
+@databaseBP.route('/deleteConnection', methods=['POST'])
+def deleteConnection():
+    try:
+        body = request.get_json()
+        connectionID = body['connectionID']
+        connection = dbConncetions.query.filter_by(connectionID=connectionID).first()
+        db.session.delete(connection)
+        db.session.commit()
+        return "Connection deleted successfully!", 200
     except Exception as e:
         return str(e), 500
--- frontEnd/static/scripts/connectionPage.js
+++ frontEnd/static/scripts/connectionPage.js
@@ -13,16 +13,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 tableBody.innerHTML = '';
                 // Populate the table with new data
                 data.forEach(connection => {
-                    console.log(connection)
                     const row = `
                         <tr>
+                            <td>${connection['connectionID']}</td>
                             <td>${connection['Host']}</td>
                             <td>${connection['Port']}</td>
                             <td>${connection['DataBase']}</td>
                             <td>${connection['UserName']}</td>
                             <td>${connection['Password']}</td>
                             <td>${connection['DatabaseType']}</td>
-
+                            <td><button class="btn" type="button" name="remove"  onclick="deleteRow(this)">Remove</button></td>
                         </tr>
                     `;
                     tableBody.insertAdjacentHTML('beforeend', row);
@@ -63,3 +63,29 @@ $('#addConnectionForm').submit(function(event) {
     });
 
 });
+
+function deleteRow(button){
+    var tableRow=button.parentNode.parentNode;
+    var rowIndex = tableRow.rowIndex
+    cells = tableRow.getElementsByTagName('td');
+    connectionID = cells[0].innerHTML
+    dataOBJ = {"connectionID": connectionID}
+    connectionIdJson = JSON.stringify(dataOBJ)
+    $.ajax({
+        url: '/deleteConnection',  // Replace with your server endpoint
+        type: 'POST',
+        contentType: 'application/json',
+
+        data: connectionIdJson,
+        success: function(response) {
+            document.getElementById('connectionTable').deleteRow(rowIndex);
+        },
+        error: function(error) {
+            console.error('Error:', error);
+            // Optionally, handle error
+        }
+    });
+
+
+
+};
